0170c4cc993d982ed77553fbd53dcabe864da3c4
LeoScopeta <65691356+LeoScopeta@users.noreply.github.com>
2022-01-30 21:33:19 -0300

    Redirect functionality added

--- src/controller/URLcontroller.ts
+++ src/controller/URLcontroller.ts
@@ -8,4 +8,15 @@ export class URLController {
         const shortURL = `${config.API_URL}/${hash}`
         response.json({  originURL, hash, shortURL})
 	}
+    public async redirect(req: Request, response: Response): Promise<void> {
+		const { hash } = req.params
+		const url = await URLModel.findOne({ hash })
+
+		if (url) {
+			response.redirect(url.originURL)
+			return
+		}
+
+		response.status(400).json({ error: 'URL not found' })
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import express from 'express'
 import { URLController } from './controller/URLController'
 
 const api = express()
-api.listen(5000, () => console.log('Express listening'))
+
 api.post('/shorten', urlController.shorten)
+api.get('/:hash', urlController.redirect)
+
+api.listen(5000, () => console.log('Express listening'))
